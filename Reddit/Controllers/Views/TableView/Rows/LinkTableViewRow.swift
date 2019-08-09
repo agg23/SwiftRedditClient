ccd14b1bc6d4d9b3bb2ccf2f4fd94cf661c6b430
Adam Gastineau <adam@appcannonsoftware.com>
2019-08-09 16:16:56 -0700

    Added score coloring

--- Reddit/Controllers/Views/TableView/Rows/LinkTableViewRow.swift
+++ Reddit/Controllers/Views/TableView/Rows/LinkTableViewRow.swift
@@ -18,20 +18,38 @@ class LinkTableViewRow: ListingTableViewRow<Link> {
         set {
             _data = newValue
             titleLabel.stringValue = _data?.title ?? ""
+            scoreLabel.stringValue = "\(data?.score ?? 0)"
+            
+            var color: NSColor = .textColor
+            
+            if let likes = data?.likes {
+                color = likes ? .orange : .blue
+            }
+            
+            scoreLabel.textColor = color
         }
     }
     
+    let scoreLabel = NSLabel()
     let titleLabel = NSLabel()
     
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
 
+        addSubview(scoreLabel)
         addSubview(titleLabel)
         
+        scoreLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         titleLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         
-        titleLabel.snp.makeConstraints { (make) in
+        scoreLabel.snp.makeConstraints { (make) in
             make.left.equalTo(self.snp.leftMargin)
+            make.top.equalTo(self.snp.topMargin)
+            make.bottom.equalTo(self.snp.bottomMargin)
+        }
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(scoreLabel.snp.right).offset(10)
             make.right.equalTo(self.snp.rightMargin)
             make.top.equalTo(self.snp.topMargin)
             make.bottom.equalTo(self.snp.bottomMargin)
